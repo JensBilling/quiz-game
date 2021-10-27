9ce77278115792b29691e2dcd35d42ea3c24f703
LisaYli <lisa.ylikangas@hotmail.com>
2021-10-27 13:12:57 +0200

    in progress

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+<button class="button">
+<slot>Submit</slot>
+</button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/BaseInput.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  // next-level vue2
+</div>
+</template>
+
+<script>
+export default {
+  name: "BaseInput"
+}
+</script>
+
+<style scoped>
+
+</style>
