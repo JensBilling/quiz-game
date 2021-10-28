5cd608405499c4a32d8f831a373dafbca23a97c0
Jens Billing <jensis92@hotmail.com>
2021-10-28 11:42:08 +0200

    Replaced Axios with Fetch, now front end can read response from back end

--- src/views/CreateUser.vue
+++ src/views/CreateUser.vue
@@ -1,59 +1,56 @@
 <template>
-<div>
-  <form id="registration-form" @submit.prevent="sendDataToBackend">
-<label for="username">
-Username:
-</label>
-   <input v-model="username" id="username" type="text" name="username">
-    <label for="userPassword">
-      Password:
-    </label>
-    <input v-model="userPassword" id="userPassword" type="text" name="userPassword">
+  <div>
+    <form id="registration-form" @submit.prevent="sendDataToBackend">
+      <label for="username">
+        Username:
+      </label>
+      <input v-model="username" id="username" type="text" name="username">
+      <label for="userPassword">
+        Password:
+      </label>
+      <input v-model="userPassword" id="userPassword" type="text" name="userPassword">
 
-    <label for="userEmail">
-      Email:
-    </label>
-    <input v-model="userEmail" id="userEmail" type="text" name="userEmil">
+      <label for="userEmail">
+        Email:
+      </label>
+      <input v-model="userEmail" id="userEmail" type="text" name="userEmil">
 
-    <button type="submit">Submit</button>
-  </form>
-</div>
+      <button type="submit">Submit</button>
+    </form>
+    <p>{{ createUserResponse }}</p>
+  </div>
 </template>
 
-
 <script>
-import axios from 'axios'
 export default {
-  data(){
+  data() {
     return {
       username: '',
-      userPassword:'',
-      userEmail: ''
+      userPassword: '',
+      userEmail: '',
+      createUserResponse: ''
 
     }
   },
   methods: {
-
     sendDataToBackend() {
-      const jsonData = {'username':this.username,'password':this.userPassword,'email':this.userEmail}
-
-      axios.post('http://127.0.0.1:3000/createuser', jsonData)
-      .then(function (response){
-        console.log('Response', response)
-        console.log('hello backend')
-        console.log(jsonData)
-      })
-      .catch(function (err){
-        console.log('Error', err)
-      })
+      const jsonData = {'username': this.username, 'password': this.userPassword, 'email': this.userEmail}
+      let options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(jsonData)
+      }
+      fetch('http://127.0.0.1:3000/createuser', options)
+          .then(response => response.json())
+          .then(data => {
+            this.createUserResponse = data.createUserStatus
+          })
     }
-
   }
-
-
 }
-
-
 </script>
 
 <style scoped>
